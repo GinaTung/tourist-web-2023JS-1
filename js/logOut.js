9ab911103fc2c2d607fa71608af89f11140d0798
yuling <gb2013429@gmail.com>
2023-10-23 19:36:02 +0800

    路徑錯誤4

--- js/logOut.js
+++ js/logOut.js
@@ -7,7 +7,7 @@ function logOut() {
   localStorage.removeItem("token"); // 使用 "token" 作為鍵名稱
   localStorage.removeItem("userId"); // 如果需要，同時刪除用戶ID
   // 跳轉到登入頁面或首頁
-  window.location.href = "${url}/viewsList.html";
+  window.location.href = "./viewsList.html";
 }
 
 
