0a63465ddce3e31a0e23cb1e7dc031ee5335655f
yuling <gb2013429@gmail.com>
2023-10-23 19:04:20 +0800

    url-update

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-// const url = "http://localhost:3000";
+const url = "https://tourist-web-2023js-1.onrender.com";
 // let id = "";
-const url = 'http://127.0.0.1:8080' || 'https://tourist-web-2023js-1.onrender.com';
+// const apiUrl = 'http://localhost:3000' || 'https://tourist-web-2023js-1.onrender.com';
 let token = localStorage.getItem("token");
 
 // token = localStorage.getItem("token")
