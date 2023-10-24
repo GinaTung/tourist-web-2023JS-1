9c0decd204bb9e96a77683ea933a89ea6bf5ff5c
yuling <gb2013429@gmail.com>
2023-10-24 11:39:04 +0800

    改url

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-// const url = "https://tourist-web-2023js-1.onrender.com";
+const url = "https://tourist-web-2023js-1.onrender.com";
 // let id = "";
-const url = 'http://localhost:3000';
+// const url = 'http://localhost:3000';
 let token = localStorage.getItem("token");
 
 // token = localStorage.getItem("token")
